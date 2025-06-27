b15e8e693e02f15e6ac9f4395bacf5961a58bcb3
codebymahak <mahaksaini4041@gmail.com>
2025-06-27 21:58:56 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,49 @@
+//math
+// console.log(Math.abs(0));//return opposite value -0
+// console.log(Math.abs("1"));
+// console.log(Math.abs(null));
+// console.log(Math.abs(""));
+// console.log(Math.abs("mahak"));
+// console.log(Math.abs({}));
+// console.log(Math.abs([2]));
+// console.log(Math.abs([1,2]));
+
+// console.log(Math.ceil(-7.004));
+// console.log(Math.ceil(4));
+// console.log(Math.ceil(0.45));
+// console.log(Math.ceil(-0.95));
+// console.log(Math.ceil(7.004));
+
+
+// console.log(Math.floor(45.05));
+// console.log(Math.ceil(45.05));
+
+// console.log(Math.cos(-0));
+// console.log(Math.cos(1));
+// console.log(Math.cos(Math.PI));
+
+
+// console.log(Math.log(11.8));
+// console.log(Math.log(1));
+
+// console.log(Math.max(1,2,5,10));
+// console.log(Math.min(1,2,5,10));
+
+// console.log(Math.pow(2,0.2));
+// console.log(Math.pow(4,0.5));
+// console.log(Math.pow(8,1/3));
+// console.log(Math.pow(2,-0.2));
+// console.log(Math.pow(-2,0.2));
+
+// console.log(Math.round(25.45));
+
+// console.log(Math.sin(180));
+
+// console.log(Math.sqrt(25));
+// console.log(Math.sqrt(2));
+
+// console.log(Math.tan(1));
+// console.log(Math.tan(90));
+
+
+
