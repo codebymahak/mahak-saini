29764e4cd00921dfa5ab177332c372e135f0a1b7
codebymahak <mahaksaini4041@gmail.com>
2025-06-27 18:44:21 +0530

    Add files via upload

---
+++ number.js
@@ -0,0 +1,41 @@
+//number is finite or not 
+// let num=37;
+// let num2=.5;
+// console.log(Number.isFinite(num,num2));//true
+// console.log(Number.isFinite(1/0));//false
+// console.log(Number.isFinite(0));//true
+// console.log(Number.isFinite(null));//false
+
+//number is integer
+//  console.log(Number.isInteger(5/110));//false
+// console.log(Number.isInteger(NaN));
+// console.log(Number.isInteger(5.000));
+
+//number is nan
+// console.log(Number.isNaN(0/0));
+// console.log(isNaN(undefined));
+// console.log(isNaN(37));
+
+//is safeInteger
+// console.log(Number.isSafeInteger(0.5));
+
+//is parsefloat
+//  let val="23.89";
+// console.log(Number.parseFloat(val));
+// console.log(Number.parseInt(val));
+//  let value="2378";
+// console.log(Number.parseFloat(value));
+// console.log(Number.parseInt(value));
+
+// console.log(Number.POSITIVE_INFINITY);
+// console.log(Number.NEGATIVE_INFINITY);
+
+// let x=Number.MAX_SAFE_INTEGER+1;
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(x);
+
+// console.log((678544).toLocaleString());
+// let x=1234;
+// console.log(typeof x);
+// console.log((0.4).toFixed(30));
